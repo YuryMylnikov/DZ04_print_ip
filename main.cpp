0553253c39e5053659db5a95d4d8d4796bfae081
YuryMylnikov <otus.learn@gmail.com>
2020-06-21 17:12:27 +0300

    initial

---
+++ main.cpp
@@ -0,0 +1,163 @@
+#include <list>
+#include <tuple>
+#include <vector>
+#include <bitset>
+#include <iostream>
+#include <type_traits>
+
+
+template<typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
+void print(T val)
+{
+	const uint8_t bits{ 8 };
+	int8_t offset = sizeof(T) - 1;
+
+	std::cout << (uint32_t)(std::numeric_limits<uint8_t>::max() & (val >> bits * offset));
+
+	for (--offset; offset >= 0; --offset)
+	{
+		std::cout << '.' << (uint32_t)(std::numeric_limits<uint8_t>::max() & (val >> bits * offset));
+	}
+}
+
+
+template<typename T, typename = std::enable_if_t<std::is_same_v<T, std::string>>>
+void print(const T& val)
+{
+	std::cout << val.c_str();
+}
+
+
+template
+<
+	template<typename...> typename T, 
+	typename... Args,
+	typename = std::enable_if_t
+	<
+		std::is_same_v
+		<
+			T<Args...>,
+			std::vector<Args...>
+		>
+
+		||
+
+		std::is_same_v
+		<
+			T<Args...>,
+			std::list<Args...>
+		>
+	>
+>
+void print(const T<Args...>& container)
+{
+	auto it = container.cbegin();
+	std::cout << *it;
+
+	++it;
+
+	for (; it != container.cend(); ++it)
+	{
+		std::cout << '.' << *it;
+	}
+}
+
+
+template<size_t index, typename Head, typename... Args>
+struct print_tuple_elements
+{
+	static void print(const std::tuple<Head, Args...>& tup)
+	{
+		print_tuple_elements<index - 1, Head, Args...>::print(tup);
+
+		std::cout << '.' << std::get<index>(tup);
+	}
+};
+
+
+template<typename Head, typename... Args>
+struct print_tuple_elements<0, Head, Args...>
+{
+	static void print(const std::tuple<Head, Args...>& tup)
+	{
+		std::cout << std::get<0>(tup);
+	}
+};
+
+
+template
+<
+	template<typename...> typename T, 
+	typename Head,
+	typename... Args,
+	typename = std::enable_if_t		
+	< 
+			std::is_same_v
+			< 
+				T<Head, Args...>,
+				std::tuple<Head, Args...>
+			>
+
+			&&
+
+			(std::is_same_v<Head, Args> && ...)
+	> 
+>
+void print(const T<Head, Args...>& tup)
+{
+	print_tuple_elements<std::tuple_size_v<T<Head, Args...>> - 1, Head, Args...>::print(tup);
+}
+
+
+template<typename T>
+void print_ip(const T& val)
+{
+	print(val);
+	std::cout << std::endl;
+}
+
+
+int main()
+{
+	{
+		char val{ -1 };
+		print_ip(val);
+	}
+
+	{
+		short val{ 0 };
+		print_ip(val);
+	}
+
+	{
+		int val{ 2130706433 };
+		print_ip(val);
+	}
+
+	{
+		long long val{ 8875824491850138409 };
+		print_ip(val);
+	}
+
+	{
+		std::string val{ "Hello" };
+		print_ip(val);
+	}
+
+	{
+		std::vector<int> val{ 1, 2, 3 };
+		print_ip(val);
+	}
+
+	{
+		std::list<int> val{ 4, 5, 6 };
+		print_ip(val);
+	}
+
+	{
+		std::tuple val{ 7, 8, 9 };
+		print_ip(val);
+	}
+
+	return 0;
+}
